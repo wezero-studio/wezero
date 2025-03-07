f41192f1ef0749f49ff4939dc21ac5c820015a7a
mdanishharoon <i220955@nu.edu.pk>
2025-03-07 21:21:46 +0500

    website fixed. awaits customisation changes

--- app/components/ProductShowcase.tsx
+++ app/components/ProductShowcase.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
-import Image from "next/image"
+import Image from "next/legacy/image"
 
 const products = [
   {
